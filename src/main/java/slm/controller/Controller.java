4cc1a10095625645dc2521326e0f7fb6cb9141ca
Fiona K <fiona5@gmx.at>
2021-05-12 10:43:16 +0200

    RestService setup finished with sample functions

---
+++ src/main/java/slm/controller/Controller.java
@@ -0,0 +1,28 @@
+package slm.controller;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/api/sample")
+public class Controller {
+    private static int counter=0;
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String SampleMethod(){
+        counter++;
+        System.out.println("Call to GET");
+        return "sample" + counter;
+    }
+    //funktioniert iwie nicht
+    @GET
+    @Path("/{delta}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String SampleMethodWithDelta(@PathParam("delta") int delta){
+        counter = counter + delta;
+        System.out.println("Call to POST");
+        return "sample" + counter;
+    }
+}
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,12 +6,8 @@ public class MainTest {
     @Test
     public void test_Given_SeeAWelcomeMessage_When_WeGetTheMessage_Then_TheMessageShouldBeWelcomeAtMaintenanceMonitor(){
         //arrange
-
         //act
-        String actual = Main.message();
-
         //assert
-        Assertions.assertEquals("Welcome at MaintanceMonitor", actual);
     }
 
 
